ba3087de6e6766f780909d5d7ff11b7d2b4f0f51
Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
2022-02-26 22:42:55 -0800

    QoL & UI
    Removed some unused social links, updated social link URLs, and made some UI adjustments for ease of useability

--- app.js
+++ app.js
@@ -12,11 +12,10 @@ let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
 
 //Paralax Effect
-
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
-    
+
     translate.forEach(element => {
         let speed = element.dataset.speed;
         element.style.transform = `translateY(${scroll * speed}px)`;
@@ -39,26 +38,26 @@ window.addEventListener('scroll', () => {
 
 //Navigation Hamburger Menu
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.navlinks');
-  const navLinks = document.querySelectorAll('.navlinks li');
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
+    const navLinks = document.querySelectorAll('.navlinks li');
 
-  //Toggles Nav
-  burger.addEventListener('click',()=>{
-      nav.classList.toggle('nav-active');
+    //Toggles Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
 
-  //Animates Links
-  navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-          link.style.animation = ''
-      } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .50}s`;
-      }
-  });
-  
-  //Burger Animation
-  burger.classList.toggle('toggle');
-});
+        //Animates Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .50}s`;
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
 }
 
 navSlide();
