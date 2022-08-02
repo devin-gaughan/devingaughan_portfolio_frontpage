70f7baa7e7c7639710e268dea44290d52535bd2f
Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
2022-08-01 19:16:53 -0700

    Squashed commit of the following:
    commit 3dd3fdc9b96a9b85ac11d017c3031d4d8b473c48
    Author: Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
    Date:   Mon Aug 1 03:12:21 2022 -0700
    
        Mobile NAV menu
    
        Fixed mobile nav menu responsiveness
        Bottom menu items were getting cut off on small display sizes
    
    commit 1ff6b3eaf445c4f2ca2af74c93d5223962cab8d0
    Author: Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
    Date:   Mon Aug 1 02:44:47 2022 -0700
    
        Responsive design changes
    
        Added nav bar for larger displays
    
    commit f04c448e00a75432f94409dff8dbcefd6767b6aa
    Author: Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
    Date:   Mon Aug 1 01:56:57 2022 -0700
    
        Removed section image
    
    commit 01e8c1bef241dc64a5a13637f420c835d227e240
    Author: Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
    Date:   Mon Aug 1 01:54:01 2022 -0700
    
        Various updates
    
        Mostly styling with CSS
    
    commit 650353a07fb71749c56ce4f2e66b77975dcc865c
    Author: Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
    Date:   Sun Jul 31 05:31:18 2022 -0700
    
        Updated mission statement
    
    commit 9bfd69e22db781e66e944dfcd67006dd3b08d008
    Author: Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
    Date:   Sun Jul 31 05:05:14 2022 -0700
    
        Transitioning to DevinGaughan
    
    commit 61db7f8271a8709859d2c60028ed72d47978ee24
    Author: Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
    Date:   Sat Mar 5 00:18:53 2022 -0800
    
        Navigation and UI changes, bugfixes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const content = document.querySelector(".content");
 const section = document.querySelector("section");
 const image_container = document.querySelector(".imgContainer");
 const opacity = document.querySelectorAll(".opacity");
+const logo = document.querySelector(".logo");
+const bignavlist = document.querySelector(".bignavlist");
 const border = document.querySelector(".border");
 
 let header_height = header.offsetHeight;
@@ -28,7 +30,10 @@ window.addEventListener('scroll', () => {
     big_title.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
-    content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
+    logo.style.opacity = - scroll / (header_height / 1.5) + 1;
+    bignavlist.style.opacity = - scroll / (header_height / 1.5) + 1;
+
+/*  content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`; */    
     image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
 
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
