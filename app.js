da6a010afe2b51798c86f1e0ed1dacf53c1ac140
Devin Gaughan <12529122+devin-gaughan@users.noreply.github.com>
2023-06-08 19:08:07 +0700

    Simplified Design

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const border = document.querySelector(".border");
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
 
-//Paralax Effect
+// //Paralax Effect
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
@@ -26,9 +26,9 @@ window.addEventListener('scroll', () => {
         element.style.transform = `translateY(${scroll * speed}px)`;
     });
 
-    /*     opacity.forEach(element => {
+         opacity.forEach(element => {
         element.style.opacity = scroll / (sectionY.top + section_height);
-        }) */
+        }); 
 
     big_title.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
@@ -37,8 +37,8 @@ window.addEventListener('scroll', () => {
 
     bignavlist.style.opacity = - scroll / (header_height / 1.5) + 1;
 
-    /*  content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`; */    
-    /*  image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`; */
+      content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;  
+      image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`; 
 
 
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
