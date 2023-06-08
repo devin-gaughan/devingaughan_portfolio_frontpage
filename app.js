60afded9853d3e70564e74da3dde66029657c7c7
Devin Gaughan <12529122+devin-gaughan@users.noreply.github.com>
2023-06-08 21:25:51 +0700

    Clean up

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const translate = document.querySelectorAll(".translate");
-const big_title = document.querySelector(".big-title");
+// const translate = document.querySelectorAll(".translate");
+// const big_title = document.querySelector(".big-title");
 const header = document.querySelector("header");
 const shadow = document.querySelector(".shadow");
 const content = document.querySelector(".content");
@@ -30,14 +30,14 @@ window.addEventListener('scroll', () => {
         element.style.opacity = scroll / (sectionY.top + section_height);
         }); 
 
-    big_title.style.opacity = - scroll / (header_height / 2) + 1;
+    // big_title.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
     logo.style.opacity = - scroll / (header_height / 1.5) + 1;
 
     bignavlist.style.opacity = - scroll / (header_height / 1.5) + 1;
 
-      content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;  
+    //   content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;  
       image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`; 
 
 
